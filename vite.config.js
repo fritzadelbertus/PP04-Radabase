13d58b46cb36657a9917fd6d38d23471785c875f
Fritz Adelbertus Sitindaon <107984452+fritzadelbertus@users.noreply.github.com>
2022-11-20 07:39:53 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PP04-Radabase/',
+  base: '/PP04_Radabase/',
   plugins: [react()],
 });
